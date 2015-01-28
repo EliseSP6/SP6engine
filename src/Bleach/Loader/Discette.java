8e75288e9a49a9313480dc69e84babd5f5c183b0
AtaDVK <ata.dvk@gmail.com>
2015-01-28 13:49:26 +0100

    Loadern

--- src/Bleach/Loader/Discette.java
+++ src/Bleach/Loader/Discette.java
@@ -1,5 +1,16 @@
 package Bleach.Loader;
 
-public class Discette {
+import java.awt.image.BufferedImage;
+import java.util.Map;
 
+public class Discette {
+	static Map<String, BufferedImage> images;
+
+	static void loadImages(String pathToJSON) {
+		// TODO
+	}
+
+	static BufferedImage getImage(String code) {
+		return images.get(code);
+	}
 }
