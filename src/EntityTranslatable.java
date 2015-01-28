86d2d874acbdc5971cf74a5c443e1d38d2a4005d
AtaDVK <ata.dvk@gmail.com>
2015-01-28 13:14:41 +0100

    EntityTranslatable

---
+++ src/EntityTranslatable.java
@@ -0,0 +1,10 @@
+import java.awt.geom.Point2D;
+
+public interface EntityTranslatable {
+
+	public Point2D.Double getPosition();
+
+	public void setPosition(Point2D.Double position);
+
+	public double getRadius();
+}
