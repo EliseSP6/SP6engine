d8c9c656049a286816492eac11e708c7f5af0e80
AtaDVK <ata.dvk@gmail.com>
2015-01-28 16:50:23 +0100

    Receptionist är klar

--- src/Bleach/InputManager/Receptionist.java
+++ src/Bleach/InputManager/Receptionist.java
@@ -1,5 +1,13 @@
 package Bleach.InputManager;
 
-public class Receptionist {
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 
+public interface Receptionist {
+	public void handleEvent(ActionEvent event);
+
+	public void handleEvent(MouseEvent event);
+
+	public void handleEvent(KeyEvent event);
 }
