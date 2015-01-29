80bdc5023c5a0a52011b22afb84c7dc09c1a08ae
EliseSP6 <git_sp6_elise@nek0.com>
2015-01-29 17:51:21 +0100

    Testing platform
    Added a runnable "Game" class, for testing purposes, including a
    simulated use case of the API.

---
+++ src/Bleach/Game.java
@@ -0,0 +1,39 @@
+package Bleach;
+
+/*
+ * This is for testing the game engine.
+ * This is where the game developer resides.
+ * 
+ * */
+
+public class Game {
+
+	public static void main(String[] args) {
+		
+		
+		Bleach myGame = new Bleach();
+		
+		myGame.loadImages("assets/images/assets.json");
+		myGame.loadSounds("assets/sounds/assets.json");
+		
+		myGame.setFPS(60);
+		
+		myGame.setSize(800, 600);
+		myGame.setTitle("My super game!");
+		
+		Level firstLevel = new Level(1800, 1600, "Town");
+		
+		firstLevel.addBackground(myGame.getTexture("clouds"));
+		firstLevel.addBackground(myGame.getTexture("sky"));
+		firstLevel.addMobile(new EntityFly(myGame.getSprite("brown_fly"), 200, 264));
+		
+		firstLevel.setMusicTrack("melody7");
+		
+		myGame.addLevel(firstLevel);
+		
+		myGame.init();
+		
+
+	}
+
+}
